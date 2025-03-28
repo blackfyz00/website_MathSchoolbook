05b58b563b84e5604348ced636b8ef368de718d9
Заславский Ярослав Максимович <blackfyz00@gmail.com>
2025-03-28 18:28:54 +0600

    searchbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import SideMenu from './components/SideMenu';
-import './styles/global.css'; // Добавляем этот импорт
+import SearchBar from './components/SearchBar';
+import './styles/global.css';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-      <SideMenu />
-
-      {/* Основное содержимое */}
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
+      <div className="app-container">
+        {/* Поиск теперь вне хедера и выше всех элементов */}
+        <div className="search-top-right">
+          <SearchBar />
+        </div>
+        
+        <div className="main-content">
+          <SideMenu />
+          <div className="page-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+        </div>
       </div>
     </Router>
   );
 }
+
 export default App;
